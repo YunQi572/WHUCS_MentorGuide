9ab3404d75dd9762e4de2ace18974dee7c19ec41
YunQi572 <YJunGuo20050702@outlook.com>
2025-03-28 10:07:03 +0800

    Update

---
+++ src/main/java/com/example/whucs_mentorguide/WhucsMentorGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.whucs_mentorguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhucsMentorGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhucsMentorGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whucs_mentorguide/WhucsMentorGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whucs_mentorguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhucsMentorGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
